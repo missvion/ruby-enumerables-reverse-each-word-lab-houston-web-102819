7723c9455348aeea68c0e2f0753bc56c046ee753
Vivian Tran <viviantran94@gmail.com>
2019-10-22 04:42:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,11 +8,11 @@ def reverse_each_word(sentence)
 end
 
 def use_collect(sentence)
-  array = string.split(" ") #turn string into an array
-  test_array = []
-  array.collect do|string| #iterate over the array
-    test_array << string.reverse #reverse each word in the array
+  array = string.split(" ")
+  new_array = []
+  array.collect do |sentence|
+    new_array << sentence.reverse
   end
-  test_array.join(" ")
+  new_array.join(" ")
 end
 
